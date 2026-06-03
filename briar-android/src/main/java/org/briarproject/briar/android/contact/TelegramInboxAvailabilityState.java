7c50e2dfc09175b22fb9ee72f704fc3303c0c5a2
amunratis <sirthesurfer@gmail.com>
2026-06-03 04:24:09 +0200

    heartbeat(harbor): convert inbox availability state to Kotlin

--- briar-android/src/main/java/org/briarproject/briar/android/contact/TelegramInboxAvailabilityState.java
+++
@@ -1,9 +0,0 @@
-package org.briarproject.briar.android.contact;
-
-enum TelegramInboxAvailabilityState {
-	NONE,
-	LOADING,
-	ACCOUNT_UNAVAILABLE,
-	EMPTY,
-	LOAD_FAILED
-}
---
+++ briar-android/src/main/kotlin/org/briarproject/briar/android/contact/TelegramInboxAvailabilityState.kt
@@ -0,0 +1,9 @@
+package org.briarproject.briar.android.contact
+
+enum class TelegramInboxAvailabilityState {
+	NONE,
+	LOADING,
+	ACCOUNT_UNAVAILABLE,
+	EMPTY,
+	LOAD_FAILED
+}
